31d56e5968634e0c2a949ea4f692fe1465e51e22
Kelmith <fayekelmith@gmail.com>
2023-11-06 12:57:01 +0530

    continued till 0:54

--- base/views.py
+++ base/views.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {"id":1, "name": "Let's code"},
+    {"id":2, "name": "Django is kinda dope"},
+    {"id":3, "name": "Except templates"},
+         ]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',{'rooms':rooms})
 
 
 def room(request):
